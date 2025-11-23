d78f38e9e463debaccefe508b75c1464dc9d05fb
shanchuan <1537521192@qq.com>
2025-11-23 22:25:36 +0800

    feat: initialize backend scaffold with openapi, spotless, jacoco and passing build

---
+++ backend/src/main/java/com/universal/qbank/ExamPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.universal.qbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamPlatformApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ExamPlatformApplication.class, args);
+  }
+}
